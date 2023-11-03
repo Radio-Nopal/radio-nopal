2c8814f6e4b6f98717393b20ea767067e0737fcf
José Carlos Pesina Siller <carlospesina@gmail.com>
2023-11-02 22:26:31 -0300

    nuevos campos de sanity

--- src/components/MixcloudArchive/MixcloudArchive.jsx
+++ src/components/MixcloudArchive/MixcloudArchive.jsx
@@ -7,7 +7,7 @@ const MixcloudArchive = ({ data }) => data.map(({ _key, descripcion, link }) => 
     key={_key}
     width="100%"
     height="60"
-    src={`https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=${link}`}
+    src={`https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=${link}/`}
     frameBorder="0"
   />
 ));
