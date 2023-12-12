fba5219454ab0105aa7fb310842381e41c9996f5
pesinasiller <pesinasiller@users.noreply.github.com>
2023-12-12 21:24:52 +0000

    Deploying to gh-pages from @ Radio-Nopal/radio-nopal@52cb05b6cf33ffbb065b819592f563784b0124cb 🚀

--- src/util/getCssColor.js
+++
@@ -1,9 +0,0 @@
-const getCssColor = (colorString = null) => {
-  const colorObject = JSON.parse(colorString);
-  if (!colorObject) {
-    return null;
-  }
-  return colorObject.css !== 'rgba(255,255,255,1)' ? colorObject.css : null;
-};
-
-export default getCssColor;
