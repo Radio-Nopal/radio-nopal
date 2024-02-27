8999a67d39d9eae1ff741608c8eb0ad797202157
José Carlos Pesina Siller <carlospesina@gmail.com>
2024-02-27 19:56:46 -0300

    dos players de streaming

--- src/components/Header/StreamingStatusIndicator.jsx
+++ src/components/Header/StreamingStatusIndicator.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { store } from '../../store';
+import './StreamingStatusIndicator.scss';
 
 function StreamingStatusIndicator() {
   const { state } = useContext(store);
@@ -9,11 +10,12 @@ function StreamingStatusIndicator() {
 
   const backgroundColor = isAnyStreamOnline ? 'bg-red-600' : 'bg-gray-600';
   const textColor = isAnyStreamOnline ? 'text-white' : 'text-black';
+  const circleClass = isAnyStreamOnline ? 'blinking fill-white' : 'fill-black';
 
   return (
     <div className={`flex justify-center items-center md:w-24 w-full select-none text-xs py-3 ${backgroundColor} ${textColor}`}>
       <svg className="w-4 h-4 mr-1" viewBox="0 0 24 24">
-        <circle cx="12" cy="12" r="8" className={textColor} />
+        <circle cx="12" cy="12" r="8" className={circleClass} />
       </svg>
       {isAnyStreamOnline ? 'EN VIVO' : 'OFFLINE'}
     </div>
