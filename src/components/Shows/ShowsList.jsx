00f8fb0bbbd7235c90d563df5abceb1da911c69e
José Carlos Pesina Siller <carlospesina@gmail.com>
2024-02-28 20:25:36 -0300

    fixes: muestra horario y nombres de locutores concatenados en lista de programas

--- src/components/Shows/ShowsList.jsx
+++ src/components/Shows/ShowsList.jsx
@@ -4,19 +4,17 @@ import { client, obtenerUrlDePrimeraImagen } from '../../util/sanityClient';
 import Loader from '../Loader';
 import './Shows.scss';
 
-function ShowsList({ searchTerm, filter }) {
+function ShowsList({ searchTerm, queryFilter = '' }) {
   const [datos, setDatos] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const search = searchTerm ? `&& titulo match '${searchTerm}'` : '';
 
   useEffect(() => {
-    const query = ` *[_type == "programa" ${search}]{ ..., locutorxs[]-> }`;
-
+    const query = ` *[_type == "programa" ${search} ${queryFilter}]{ ..., locutorxs[]-> {nombre} }`;
     client
       .fetch(query)
       .then((datosDeProgramas) => {
-        const datosFiltrados = filter ? datosDeProgramas.filter(filter) : datosDeProgramas;
-        setDatos(datosFiltrados);
+        setDatos(datosDeProgramas);
         setIsLoading(false);
       })
       .catch((err) => console.error(err));
@@ -28,12 +26,20 @@ function ShowsList({ searchTerm, filter }) {
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         {datos.map((programa) => {
           const {
-            slug, titulo, dias, hora, periodicidad, locutorxs, imagenes,
+            slug, titulo, dias, hora, periodicidad, locutorxs, imagenes, fechasLegacy,
           } = programa;
           const key = slug.current;
 
+          // arregla el problema de que a veces el horario está escrito sin espacios entre los |
+          const formatedFechasLegacy = fechasLegacy
+            ?.split('|')
+            .map((part) => part.trim()) // Remove leading and trailing spaces
+            .join(' | ');
+
+          const horario = dias && hora && periodicidad ? `${dias} | ${hora} | ${periodicidad}` : formatedFechasLegacy;
+
           return (
-            <div key={key} className="show mt-6">
+            <div key={key} className="programa mt-6">
               <Link to={`/${key}`} title={titulo}>
                 <div
                   className="bg-cover bg-center md:h-32 h-52"
@@ -42,16 +48,16 @@ function ShowsList({ searchTerm, filter }) {
                     backgroundColor: 'black',
                   }}
                 />
-                <h1 className="show__title text-base truncate">{titulo}</h1>
-                {locutorxs.length ? (
-                  <span className="show__host">
-                    {`Por: ${locutorxs[0].nombre}`}
-                  </span>
+                <h1 className="programa__title text-base truncate">{titulo}</h1>
+                {locutorxs?.length && locutorxs[0]?.nombre ? (
+                  <div className="truncate block max-w-full">
+                    {`Por: ${locutorxs?.map((locutor) => locutor.nombre).join(', ')}`}
+                  </div>
                 ) : (
                   <br />
                 )}
-                {dias && hora && periodicidad ? (
-                  <div className="show__description text-left text-gray-400 text-sm">{`${dias} | ${hora} | ${periodicidad}`}</div>
+                {horario.length ? (
+                  <div className="show__description text-left text-gray-400 text-sm">{horario}</div>
                 ) : (
                   <br />
                 )}
