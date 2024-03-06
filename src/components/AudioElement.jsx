539ae9d84e340a0f39b3e977964958ce377500fe
José Carlos Pesina Siller <carlospesina@gmail.com>
2024-03-05 23:14:00 -0300

    debug player

--- src/components/AudioElement.jsx
+++ src/components/AudioElement.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 
 function AudioElement() {
   return (
-    <>
-      <audio id="audio-player-1" type="audio/mpeg" src={process.env.REACT_APP_STREAM_URL_1} />
-      <audio id="audio-player-2" type="audio/mpeg" src={process.env.REACT_APP_STREAM_URL_2} />
-    </>
+    <audio id="audio-player-1" type="audio/mpeg" src={process.env.REACT_APP_STREAM_URL_1} />
   );
 }
 
