281af11d408314b81b728cc37820e1e67efd01aa
José Carlos Pesina Siller <carlospesina@gmail.com>
2024-03-05 21:25:42 -0300

    router fix

--- src/MainRouter.jsx
+++ src/MainRouter.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import GenericPage from './pages/GenericPage';
 import Programa from './pages/Programa';
 import Persona from './pages/Persona';
@@ -26,16 +26,16 @@ function MainRouter() {
   return (
     <BrowserRouter basename="/">
       <ScrollToTop />
-      <Routes>
-        <Route path="/info/:slug" element={<GenericPage />} />
-        <Route exact path="/somos/:slug" element={<Persona />} />
+      <Switch>
+        <Route exact path="/info/:slug" element={<GenericPage />} />
+        <Route path="/somos/:slug" element={<Persona />} />
         <Route path="/busqueda/:searchTerm" element={<SearchResults />} />
         <Route path="/archivo" element={<Archivo />} />
         <Route path="/somos" element={<Somos />} />
         <Route path="/programacion" element={<Programacion />} />
         <Route path="/:slug" element={<Programa />} />
         <Route exact path="/" element={<Home />} />
-      </Routes>
+      </Switch>
     </BrowserRouter>
   );
 }
