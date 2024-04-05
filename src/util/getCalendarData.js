c48f8068657ff9e205f4cd70c7c379d577ed7086
José Carlos Pesina Siller <carlospesina@gmail.com>
2024-04-05 11:12:14 -0300

    fix playing now

--- src/util/getCalendarData.js
+++ src/util/getCalendarData.js
@@ -5,11 +5,16 @@ const browserTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 function getCalendarData(dispatch) {
   const fetchStreamData = async (stream) => {
     const { streamingId, calendarId, calendarApiKey } = stream;
-    const calendarUrl = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${calendarApiKey}&timeMin=${new Date().toISOString()}&timeMax=${new Date(Date.now() + 3600000).toISOString()}&timeZone=${browserTimeZone}`;
+
+    const startTime = `${new Date().toISOString().split('.')[0]}Z`; // '2024-04-05T13:36:47.755Z';
+
+    const endTime = `${new Date(Date.now() + 3600000).toISOString().split('.')[0]}Z`;
+    const calendarUrl = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${calendarApiKey}&timeMin=${startTime}&timeMax=${endTime}&timeZone=${browserTimeZone}`;
 
     try {
       const response = await fetch(calendarUrl);
       const data = await response.json();
+      console.log({ data });
       const nowPlaying = data.items[0]?.summary;
       dispatch({ type: 'nowPlaying', payload: nowPlaying, streamingId });
     } catch (error) {
