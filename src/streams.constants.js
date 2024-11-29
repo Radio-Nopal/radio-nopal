61c095b9c09cc952ac142a5490eabe8bbd366b6e
José Carlos Pesina Siller <carlospesina@gmail.com>
2024-11-29 23:55:41 +0300

    update_stream_name

--- src/streams.constants.js
+++ src/streams.constants.js
@@ -7,7 +7,7 @@ const streams = [
   },
   {
     streamingId: 2,
-    streamName: '6171d410f54e',
+    streamName: 'nopalVentana',
     calendarId: process.env.REACT_APP_CALENDAR_ID_2,
     calendarApiKey: process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY_2,
   },
