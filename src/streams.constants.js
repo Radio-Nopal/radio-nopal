e8d9931798768cbb3ba61a820efa158d34aa3d78
Carlos Pesina <pesinasiller@users.noreply.github.com>
2026-04-07 15:11:59 +0500

    quitar player 3

--- src/streams.constants.js
+++ src/streams.constants.js
@@ -11,12 +11,14 @@ const streams = [
     calendarId: process.env.REACT_APP_CALENDAR_ID_2,
     calendarApiKey: process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY,
   },
+  /*
   {
     streamingId: 3,
     streamName: '6171d410f54e',
     calendarId: process.env.REACT_APP_CALENDAR_ID_2,
     calendarApiKey: process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY,
   },
+  */
 ];
 
 export default streams;
